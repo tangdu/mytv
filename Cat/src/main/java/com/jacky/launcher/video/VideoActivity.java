c6d52c5636da75d1f0fd2e93769d47f384f50a1a
唐杜 <tangdu0228yes@163.com>
2018-07-10 16:22:28 +0800

    升级

--- Cat/src/main/java/com/jacky/launcher/video/VideoActivity.java
+++ Cat/src/main/java/com/jacky/launcher/video/VideoActivity.java
@@ -8,18 +8,20 @@ import android.util.Log;
 import android.view.MotionEvent;
 
 import com.jacky.launcher.R;
-import com.jacky.launcher.detail.MediaModel;
+import com.pili.pldroid.player.AVOptions;
+import com.pili.pldroid.player.widget.PLVideoTextureView;
+import com.pili.pldroid.player.widget.PLVideoView;
 
-import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 
-public class VideoActivity extends AppCompatActivity implements PlayerManager.PlayerStateListener {
+public class VideoActivity extends AppCompatActivity  {
 
     private String url1 = "https://fzjy-oss-bucket.oss-cn-shenzhen.aliyuncs.com/upload/resData/20170516/5hSFSwTGKa.mp4";
     private String url2 = "http://zv.3gv.ifeng.com/live/zhongwen800k.m3u8";
     private String url3 = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov";
-    private String url4 = "http://cn2.okokzyyun.com/20171211/5ynbwi6C/800kb/hls/index.m3u8";
-    private PlayerManager player;
+    private String url4 = "http://yunxianchang.live.ujne7.com/vod-system-bj/44_176_20170224113626af3a75cd-3508-4bc3-b51f-366fca3c7e39.m3u8";
+    private PLVideoTextureView mVideoView;
+    private MediaController mMediaController;
     public static final String VIDEO = "Video";
 
     @Override
@@ -30,35 +32,17 @@ public class VideoActivity extends AppCompatActivity implements PlayerManager.Pl
     }
 
     private void initPlayer() {
-        player = new PlayerManager(this);
-        player.setFullScreenOnly(true);
-        player.setScaleType(PlayerManager.SCALETYPE_FILLPARENT);
-        player.playInFullScreen(true);
-        player.setPlayerStateListener(this);
-        player.play(url4);
+        AVOptions options = new AVOptions();
+        mVideoView = (PLVideoTextureView) findViewById(R.id.video_player);
+        mMediaController = new MediaController(this);
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.setAVOptions(options);
+        mVideoView.setVideoPath(url4);
+        mVideoView.start();
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (player.gestureDetector.onTouchEvent(event))
-            return true;
-        return super.onTouchEvent(event);
+    protected void onDestroy() {
+        super.onDestroy();
     }
-
-    @Override
-    public void onComplete() {
-    }
-
-    @Override
-    public void onError() {
-    }
-
-    @Override
-    public void onLoading() {
-    }
-
-    @Override
-    public void onPlay() {
-    }
-
 }
